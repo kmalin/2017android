cd692ada27d50a702d1e690719a5750720e56fa8
audrius.masiulionis <audrius.masiulionis@visma.com>
2017-10-21 18:16:05 +0300

    created second lesson project and added calculator task

---
+++ 2-pamoka/Lesson/app/src/main/java/lt/birziska/monika/lesson/activities/MainActivity.java
@@ -0,0 +1,185 @@
+package lt.birziska.monika.lesson.activities;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import lt.birziska.monika.lesson.Modules.DrawerMenuItem;
+import lt.birziska.monika.lesson.fragments.CalculatorFragment;
+import lt.birziska.monika.lesson.fragments.SecondFragment;
+
+import lt.birziska.monika.lesson.R;
+
+public class MainActivity extends AppCompatActivity{
+
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+
+    // index to identify current nav menu item
+    public static int navItemIndex = 0;
+
+    // toolbar titles respected to selected nav menu item
+    private DrawerMenuItem[] menuItems;
+
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mHandler = new Handler();
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        menuItems = new DrawerMenuItem[]{
+               new DrawerMenuItem("Calculator page", "calculator"),
+               new DrawerMenuItem("Second page", "second")
+        };
+
+        setUpNavigationView();
+        addMenuItemInNavMenuDrawer();
+
+        if (savedInstanceState == null) {
+            navItemIndex = 0;
+            loadFragments();
+        }
+    }
+
+    /***
+     * Returns respected fragment that user
+     * selected from navigation menu
+     */
+    private void loadFragments() {
+        // selecting appropriate nav menu item
+        selectNavMenu();
+
+        // set toolbar title
+        setToolbarTitle();
+
+        // if user select the current navigation menu again, don't do anything
+        // just close the navigation drawer
+        if (getSupportFragmentManager().findFragmentByTag(menuItems[navItemIndex].getTag()) != null) {
+            drawer.closeDrawers();
+        }
+
+        // Sometimes, when fragment has huge data, screen seems hanging
+        // when switching between navigation menus
+        // So using runnable, the fragment is loaded with cross fade effect
+        // This effect can be seen in GMail app
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // update the main content by replacing fragments
+                Fragment fragment = getFragments();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, menuItems[navItemIndex].getTag());
+                fragmentTransaction.commitAllowingStateLoss();
+            }
+        };
+
+        // If mPendingRunnable is not null, then add to the message queue
+        if (mPendingRunnable != null) {
+            mHandler.post(mPendingRunnable);
+        }
+
+        //Closing drawer on item click
+        drawer.closeDrawers();
+
+        // refresh toolbar menu
+        invalidateOptionsMenu();
+    }
+
+    private Fragment getFragments() {
+        switch (navItemIndex) {
+            case 0:
+                CalculatorFragment calculatorFragment = new CalculatorFragment();
+                return calculatorFragment;
+            case 1:
+                SecondFragment secondFragment = new SecondFragment();
+                return secondFragment;
+            default:
+                return new CalculatorFragment();
+        }
+    }
+
+    private void setToolbarTitle() {
+        getSupportActionBar().setTitle(menuItems[navItemIndex].getTitle());
+    }
+
+    private void selectNavMenu() {
+        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+    }
+
+    private void setUpNavigationView() {
+        navigationView.setNavigationItemSelectedListener(new DrawerItemClickListener());
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+
+        if (navItemIndex != 0) {
+            navItemIndex = 0;
+            loadFragments();
+            return;
+        }
+
+        super.onBackPressed();
+    }
+
+    private void addMenuItemInNavMenuDrawer() {
+        Menu menu = navigationView.getMenu();
+        for(int i=0; i<menuItems.length; i++){
+            menu.add(0, i, Menu.NONE, menuItems[i].getTitle());
+        }
+        menu.setGroupCheckable(0, true, true);
+
+        navigationView.invalidate();
+    }
+
+    private class DrawerItemClickListener
+            implements NavigationView.OnNavigationItemSelectedListener {
+
+        @SuppressWarnings("StatementWithEmptyBody")
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
+
+            navItemIndex = id;
+            if (item.isChecked()) {
+                item.setChecked(false);
+            } else {
+                item.setChecked(true);
+            }
+
+            loadFragments();
+            return true;
+        }
+    }
+}
---
+++ 2-pamoka/Lesson/app/src/main/java/lt/birziska/monika/lesson/fragments/CalculatorFragment.java
@@ -0,0 +1,84 @@
+package lt.birziska.monika.lesson.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import lt.birziska.monika.lesson.R;
+
+public class CalculatorFragment extends Fragment {
+
+    EditText firstInput;
+    EditText secondInput;
+    TextView resultTextView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.calculator_fragment, container, false);
+
+        //set result text view
+        resultTextView = (TextView) rootView.findViewById(R.id.resultTextView);
+        //set inputs
+        firstInput = (EditText) rootView.findViewById(R.id.firstInput);
+        secondInput = (EditText) rootView.findViewById(R.id.secondInput);
+
+        //set button listeners
+        Button addBtn =  (Button)rootView.findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new AddButtonClick());
+        Button subtractBtn =  (Button)rootView.findViewById(R.id.subtractBtn);
+        subtractBtn.setOnClickListener(new SubtractButtonClick());
+        Button multiplyBtn =  (Button)rootView.findViewById(R.id.multiplyBtn);
+        multiplyBtn.setOnClickListener(new MultiplyButtonClick());
+        Button divideBtn =  (Button)rootView.findViewById(R.id.divideBtn);
+        divideBtn.setOnClickListener(new DivideButtonClick());
+
+        return rootView;
+    }
+
+    class AddButtonClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Double firstInputValue = Double.parseDouble(firstInput.getText().toString());
+            Double secondInputValue = Double.parseDouble(secondInput.getText().toString());
+            Double result = firstInputValue + secondInputValue;
+            resultTextView.setText(result.toString());
+        }
+    }
+
+    class SubtractButtonClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Double firstInputValue = Double.parseDouble(firstInput.getText().toString());
+            Double secondInputValue = Double.parseDouble(secondInput.getText().toString());
+            Double result = firstInputValue - secondInputValue;
+            resultTextView.setText(result.toString());
+        }
+    }
+
+    class MultiplyButtonClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Double firstInputValue = Double.parseDouble(firstInput.getText().toString());
+            Double secondInputValue = Double.parseDouble(secondInput.getText().toString());
+            Double result = firstInputValue * secondInputValue;
+            resultTextView.setText(result.toString());
+        }
+    }
+
+    class DivideButtonClick implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Double firstInputValue = Double.parseDouble(firstInput.getText().toString());
+            Double secondInputValue = Double.parseDouble(secondInput.getText().toString());
+            Double result = firstInputValue / secondInputValue;
+            resultTextView.setText(result.toString());
+        }
+    }
+
+}
