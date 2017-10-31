5e3bc2191db08e6b814f54f49674d3205b06a951
Kestas <kmalin@gmail.com>
2017-10-31 07:36:45 +0200

    Deleted the implementation and provided instructions for the students

--- 3-pamoka/Minisweeper/app/src/main/java/lt/birziska/minisweeper/Game.java
+++ 3-pamoka/Minisweeper/app/src/main/java/lt/birziska/minisweeper/Game.java
@@ -1,8 +1,10 @@
 package lt.birziska.minisweeper;
 
 import android.graphics.Color;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
@@ -12,13 +14,35 @@ import android.widget.TextView;
 
 public class Game extends AppCompatActivity {
 
+    // size of the grid of the crates
+    // if size is 5, then the grid will be of size 5 * 5, 25 crates in total
     int size = 5;
+
+    // how many bombs to plant when the game is generated
     int bombCount = 7;
+
+    // how many crates needs to be opened for victory
+    int createsToOpen = 10;
+
+    // two-dimensional boolean array of size specified above
+    // if value is true, then the cell has a bomb
+    // if value is false, then the cell doesn't have a bomb
     boolean[][] bombArray;
 
+    // one-dimensional array of length - size * size
+    boolean[] wasChecked;
+
+    // represents the state that the game has ended
+    // after initGame method is called, gameEnded value becomes false
+    // after setGameLost or setGameWon are called, gameEnded value becomes true
     boolean gameEnded = false;
+
+    // keeps track of the number of opened crates
     int cratesOpened = 0;
 
+    /*
+    Method that initializes view on the start of the activity.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,81 +59,81 @@ public class Game extends AppCompatActivity {
         initGame();
     }
 
-    class CrateClick implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
+    /*
+    Method that handles the click of the crate.
+     */
+    public void onCreateClick(View v){
+        ImageButton button = (ImageButton)v;
+        // id of the button has values from 0 to size * size - 1
+        int id = button.getId();
 
-            if (gameEnded) {
-                return;
-            }
+        // get the row and the column of the cell
 
-            ImageButton button = (ImageButton)v;
+        // if game is ended, then nothing should be done in this event
+        // if you already clicked on this crate before, then nothing should be done in this event
 
-            int id = button.getId();
-            int row = id / size;
-            int column = id % size;
+        // check if there is a bomb in this cell
+        // if there is a bomb, then set the bomb image and end the game with a failure state
 
-            boolean hasBomb = bombArray[row][column];
-
-            if (hasBomb) {
-                button.setImageResource(R.drawable.bomb_200);
-                gameLost();
-            } else {
-                cratesOpened++;
-                TextView cratesOpenedCount = (TextView) findViewById(R.id.cratesOpenedCount);
-                cratesOpenedCount.setText(Integer.toString(cratesOpened));
-
-                int count = calculateCountOfNeighbouringBombs(row, column);
-
-                int restId = getResourceIdByCount(count);
-                button.setImageResource(restId);
-
-                if (cratesOpened > 10) {
-                    gameWon();
-                }
-            }
-        }
+        // if there isn't a bomb in this cell, then:
+        // calculate and show opened crates count
+        // put appropriate image on the image button
+        // if victory condition is reached, end the game with the winning state
     }
 
+    /*
+    Gets the image resource for an empty crate. There won't be a bomb in it.
+    If there is no neighbouring bombs, then there should be no image.
+    In this case value 0 removes the crate image.
+    If there are 1 to 8 neighbouring bombs, then appropriate image should be returned.
+     */
     private int getResourceIdByCount(int count) {
 
         switch (count){
             case 1:
-                return R.drawable.count_1;
+                return R.drawable.count_one;
             case 2:
-                return R.drawable.count_2;
+                return R.drawable.count_two;
             case 3:
-                return R.drawable.count_3;
+                return R.drawable.count_three;
             case 4:
-                return R.drawable.count_4;
+                return R.drawable.count_four;
             case 5:
-                return R.drawable.count_5;
+                return R.drawable.count_five;
             case 6:
-                return R.drawable.count_6;
+                return R.drawable.count_six;
             case 7:
-                return R.drawable.count_7;
+                return R.drawable.count_seven;
             case 8:
-                return R.drawable.count_8;
+                return R.drawable.count_eight;
             default:
                 return 0;
         }
     }
 
-    private int calculateCountOfNeighbouringBombs(int row, int column){
+    /*
+    Calculates the number of bombs in all 8 neighbouring cells
+     */
+    private int bombCountInNeighbouringCells(int row, int column){
         int bombCount =
-            hasBomb(row - 1, column - 1) +
-            hasBomb(row - 1, column) +
-            hasBomb(row - 1, column + 1) +
-            hasBomb(row, column - 1) +
-            hasBomb(row, column + 1) +
-            hasBomb(row + 1, column - 1) +
-            hasBomb(row + 1, column) +
-            hasBomb(row + 1, column + 1);
+            bombCountInACell(row - 1, column - 1) +
+            bombCountInACell(row - 1, column) +
+            bombCountInACell(row - 1, column + 1) +
+            bombCountInACell(row, column - 1) +
+            bombCountInACell(row, column + 1) +
+            bombCountInACell(row + 1, column - 1) +
+            bombCountInACell(row + 1, column) +
+            bombCountInACell(row + 1, column + 1);
 
         return bombCount;
     }
 
-    private int hasBomb(int row, int column){
+    /*
+    Checks how many bombs does the cell have.
+    If cell is out of bounds or has no bombs, then it returns 0.
+    If cell has a bomb, then it returns 1.
+     */
+    private int bombCountInACell(int row, int column){
         if (row >= size || row < 0){
             return 0;
         }
@@ -119,24 +143,11 @@ public class Game extends AppCompatActivity {
         return bombArray[row][column] ? 1 : 0;
     }
 
-    private void initGame() {
-        View gameOverLayout = findViewById(R.id.gameOverLayout);
-        gameOverLayout.setVisibility(View.INVISIBLE);
-
-        cratesOpened = 0;
-        TextView cratesOpenedCount = (TextView) findViewById(R.id.cratesOpenedCount);
-        cratesOpenedCount.setText(Integer.toString(cratesOpened));
-
-        GridLayout layout = (GridLayout) findViewById(R.id.gameGridLayout);
-        layout.removeAllViewsInLayout();
-
-        fillInCrates(layout);
-        fillInBombArray();
-
-        gameEnded = false;
-    }
-
-    private void gameLost() {
+    /*
+    Sets the game is lost state.
+    Shows the game over text. And sets gameEnded flag.
+     */
+    private void setGameLost() {
         TextView gameOverTextView = (TextView) findViewById(R.id.gameOverTextView);
         gameOverTextView.setText("You lost.");
 
@@ -146,7 +157,11 @@ public class Game extends AppCompatActivity {
         gameEnded = true;
     }
 
-    private void gameWon() {
+    /*
+    Sets the game is won state.
+    Shows the game over text. And sets gameEnded flag.
+     */
+    private void setGameWon() {
         TextView gameOverTextView = (TextView) findViewById(R.id.gameOverTextView);
         gameOverTextView.setText("You won.");
 
@@ -156,6 +171,36 @@ public class Game extends AppCompatActivity {
         gameEnded = true;
     }
 
+    /*
+    Initializes the game.
+     */
+    private void initGame() {
+        // hides game over text
+        View gameOverLayout = findViewById(R.id.gameOverLayout);
+        gameOverLayout.setVisibility(View.INVISIBLE);
+
+        // resets cratesOpened count
+        cratesOpened = 0;
+        TextView cratesOpenedCount = (TextView) findViewById(R.id.cratesOpenedCount);
+        cratesOpenedCount.setText(Integer.toString(cratesOpened));
+
+        // clears all existing crates in the grid
+        GridLayout layout = (GridLayout) findViewById(R.id.gameGridLayout);
+        layout.removeAllViewsInLayout();
+
+        // Fills in new creates
+        fillInCrates(layout);
+        // Generates bombs
+        fillInBombArray();
+
+        gameEnded = false;
+    }
+
+    /*
+    Generates bombs array.
+    Creates two-dimensional array for bombs.
+    And one dimensional array for registering already checked crates.
+     */
     private void fillInBombArray() {
         // initialize bomb array
         bombArray = new boolean[size][size];
@@ -163,41 +208,71 @@ public class Game extends AppCompatActivity {
             bombArray[i] = new boolean[size]; // initially there will be no bombs
         }
 
+        // creates another array with all of the indexes
         int sizeSquared = size * size;
         int[] bombSelector = new int[sizeSquared];
         for (int i = 0; i < sizeSquared; i++){
             bombSelector[i] = i;
         }
         for (int i = 0; i < bombCount; i++){
+            // selects one item from the array of indexes
+            // each iteration takes the indexes from the shorter and shorter array
             int selectedIndex = (int)(Math.random() * (sizeSquared - i));
+            // gets the index
             int bombIndex = bombSelector[selectedIndex];
 
             int row = bombIndex / size;
             int column = bombIndex % size;
-            bombArray[row][column] = true; // plant a bomb
-
+            // plants the bomb
+            bombArray[row][column] = true;
+            // move the index that was not picked from the end of the array to the place
+            // where picked up index was located before
+            // next iteration in the cycle will select from one elemement smaller array
             bombSelector[selectedIndex] = bombSelector[sizeSquared - i - 1];
         }
+
+        // initializes array for registering already checked creates
+        wasChecked = new boolean[size * size];
     }
 
+    /*
+    Generates crates UI elements
+     */
     private void fillInCrates(GridLayout layout) {
+
+        // measure the width of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+        Point displaySize = new Point();
+        display.getSize(displaySize);
+        int width = displaySize.x;
+        int height = displaySize.y;
+        // leave some space for global padding
+        int layoutWidth = width - 32;
+        // leave 2 + 2 pixes for the space between crates
+        int singleCrateWidth = layoutWidth / size - 4;
 
         for(int row = 0; row < size; row++){
             for(int column = 0; column < size; column++){
                 ImageButton crate = new ImageButton(this);
                 crate.setImageResource(R.drawable.crate_200);
-                crate.setOnClickListener(new CrateClick());
+                crate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onCreateClick(v);
+                    }
+                });
                 crate.setBackgroundColor(Color.TRANSPARENT);
                 crate.setScaleType(ImageView.ScaleType.FIT_CENTER);
                 crate.setPadding(2,2,2,2);
+                // put appropriate index on to the crate button
                 int id = row * size + column;
                 crate.setId(id);
 
                 GridLayout.Spec rowSpec = GridLayout.spec(row);
                 GridLayout.Spec columnSpec = GridLayout.spec(column);
                 GridLayout.LayoutParams params = new GridLayout.LayoutParams(rowSpec, columnSpec);
-                params.width = 200;
-                params.height = 200;
+                params.width = singleCrateWidth;
+                params.height = singleCrateWidth;
                 layout.addView(crate, params);
             }
         }
