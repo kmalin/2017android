f670a6935a42027b7afe60068750646504b5ea32
Monika <monika.staniute@outlook.com>
2017-11-27 22:42:07 +0200

    Added edit grocery activity and floating action button

---
+++ 5-pamoka/GroceriesList/app/src/main/java/lt/birziska/grocerylist/Activities/EditGroceryActivity.java
@@ -0,0 +1,17 @@
+package lt.birziska.grocerylist.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import lt.birziska.grocerylist.R;
+
+/**
+ * Created by Predator on 2017-11-27.
+ */
+
+public class EditGroceryActivity  extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grocery_list);
+    }
+}
--- 5-pamoka/GroceriesList/app/src/main/java/lt/birziska/grocerylist/Activities/GroceryListActivity.java
+++ 5-pamoka/GroceriesList/app/src/main/java/lt/birziska/grocerylist/Activities/GroceryListActivity.java
@@ -1,12 +1,19 @@
-package lt.birziska.grocerylist;
+package lt.birziska.grocerylist.Activities;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+
+import lt.birziska.grocerylist.GroceriesList;
+import lt.birziska.grocerylist.Grocery;
+import lt.birziska.grocerylist.GroceryAdapter;
+import lt.birziska.grocerylist.R;
 
 public class GroceryListActivity extends AppCompatActivity {
 
@@ -16,11 +23,23 @@ public class GroceryListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocery_list);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new FabClickListener() );
+
         groceryListView = (ListView) findViewById(R.id.grocery_list_view);
 
         ArrayList<Grocery> groceryList = GroceriesList.getGroceriesFromFile(this);
 
         GroceryAdapter adapter = new GroceryAdapter(this, groceryList);
         groceryListView.setAdapter(adapter);
+    }
+
+    private class FabClickListener
+            implements View.OnClickListener {
+
+        @Override
+        public void onClick(View item) {
+            // go to grocery creation
+        }
     }
 }
