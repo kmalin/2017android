f47881855baf63af27a5aef9fb65eb97768f1a3b
Kestas <kmalin@gmail.com>
2017-12-19 00:29:38 +0200

    Added more message types

--- 6-pamoka/app/src/main/java/com/example/audriusmasiulionis/greetinggenerator/ChristmasGreeting.java
+++ 6-pamoka/app/src/main/java/com/example/audriusmasiulionis/greetinggenerator/ChristmasGreeting.java
@@ -1,54 +1,14 @@
 package com.example.audriusmasiulionis.greetinggenerator;
 
 public class ChristmasGreeting {
-    private final String friendsName;
-    private final String friendsPhoneNumber;
-    private boolean includeNewYearGreet;
-    private boolean includeBestWishesForFamily;
-    private boolean includeWarmthGreet;
 
-    private final String newYearGreeting = "Taip pat noriu pasveikinti tave su artėjančiais Naujaisais metais.";
-    private final String warmthGreeting = "Norėčiau palinkėti šilumos.";
-    private final String wishesForFamily = "Taip pat nepamiršk praleisti laiką su artimais žmonėmis";
+    private String friendsName;
+    private boolean isNewYearGreeting;
+    private String extraText;
 
+    public String getMessageBody() {
 
-    public ChristmasGreeting(String friendsName, String friendsPhoneNumber) {
-        this.friendsName = friendsName;
-        this.friendsPhoneNumber = friendsPhoneNumber;
-    }
-
-    public String getFriendsPhoneNumber() {
-        return this.friendsPhoneNumber;
-    }
-
-    public void addNewYearsGreet(){
-        this.includeNewYearGreet = true;
-    }
-
-    public void addWarmthGreet() {
-        this.includeWarmthGreet = true;
-    }
-
-    public void addBestWishesForFamily() {
-        this.includeBestWishesForFamily = true;
-    }
-
-    @Override
-    public String toString() {
-        String greeting = String.format("Labas %s, \nNoriu palinkėti gražių Šv.Kalėdų.", this.friendsName);
-
-        if (includeNewYearGreet){
-           greeting = greeting.concat("\n" + newYearGreeting);
-        }
-
-        if (includeWarmthGreet){
-            greeting = greeting.concat("\n" + warmthGreeting);
-        }
-
-        if (includeBestWishesForFamily){
-            greeting = greeting.concat("\n" + wishesForFamily);
-        }
-
-        return greeting;
+        // Implement the method
+        return "Test";
     }
 }
--- 6-pamoka/app/src/main/java/com/example/audriusmasiulionis/greetinggenerator/GreetingActivity.java
+++ 6-pamoka/app/src/main/java/com/example/audriusmasiulionis/greetinggenerator/GreetingActivity.java
@@ -6,71 +6,98 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
-import android.widget.TextView;
 
 public class GreetingActivity extends AppCompatActivity {
 
-    private ChristmasGreeting greeting;
     private Switch newYearSwitch;
-    private Switch warmthSwitch;
-    private Switch familySwitch;
-    private Button sendButton;
     private EditText nameEditView;
-    private EditText numberEditView;
+    private EditText extraTextEditView;
+
+    private ChristmasGreeting getGreeting() {
+
+        String friendsName = nameEditView.getText().toString();
+        boolean isNewYearGreeting = newYearSwitch.isChecked();
+        String extraMessage = extraTextEditView.getText().toString();
+
+        // Create ChristmasGreeting object and return it here
+
+        return null;
+    }
+
+    private void sendSms() {
+
+        ChristmasGreeting greeting = getGreeting();
+        if (greeting == null) {
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", "", null));
+        intent.putExtra("sms_body", greeting.getMessageBody());
+        startActivity(intent);
+    }
+
+    private void sendEmail() {
+
+        ChristmasGreeting greeting = getGreeting();
+        if (greeting == null) {
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[0]);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Sveikinimas");
+        intent.putExtra(Intent.EXTRA_TEXT, greeting.getMessageBody());
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void sendMessage() {
+
+        ChristmasGreeting greeting = getGreeting();
+        if (greeting == null) {
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, greeting.getMessageBody());
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greeting);
 
+        findViewById(R.id.smsButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendSms();
+            }
+        });
+        findViewById(R.id.emailButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendEmail();
+            }
+        });
+        findViewById(R.id.messageButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+
         //set the views
         newYearSwitch = findViewById(R.id.newYearSwitch);
-        warmthSwitch = findViewById(R.id.warmthSwitch);
-        familySwitch = findViewById(R.id.familySwitch);
-        sendButton = findViewById(R.id.button);
-        sendButton.setOnClickListener(new OnClick());
         nameEditView = findViewById(R.id.nameEditText);
-        numberEditView = findViewById(R.id.phoneEditText);
-    }
-
-
-    private class OnClick implements View.OnClickListener {
-        public void onClick(View view) {
-            greeting = new ChristmasGreeting(nameEditView.getText().toString(), numberEditView.getText().toString());
-
-            if (newYearSwitch.isChecked()){
-                greeting.addNewYearsGreet();
-            }
-
-            if (warmthSwitch.isChecked()){
-                greeting.addWarmthGreet();
-            }
-
-            if (familySwitch.isChecked()){
-                greeting.addBestWishesForFamily();
-            }
-
-            sendSms(greeting.getFriendsPhoneNumber(), greeting.toString());
-        }
-    }
-
-
-    //DO NOT TOUCH!!!!!
-    private void sendSms(String number, String smsContent){
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", number, null));
-        intent.putExtra("sms_body", smsContent);
-        startActivity(intent);
+        extraTextEditView = findViewById(R.id.extraTextEditText);
     }
 
 }
-
-
-
-
-
-
-
-
